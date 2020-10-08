c9b56d516a21e9c7020b6a1e31a4a0771c39c2a9
andi194 <58745968+andi194@users.noreply.github.com>
2020-10-08 14:42:06 +0100

    redux v1
    redux v1

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from "redux"
+import { rootReducer } from './reducer'
+
+function loadState() {
+  const serializedState = localStorage.getItem('state');
+  if (!serializedState) {
+    return undefined;
+  }
+  return JSON.parse(serializedState);
+};
+
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+function saveState(state) {
+
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
+} 
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
